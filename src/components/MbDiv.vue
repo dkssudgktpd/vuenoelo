212cfc6515ada450e913bdcbead632b858fba6bf
moon <anstnqls554@gmail.com>
2022-08-23 10:52:00 +0900

    뷰 완성

---
+++ src/components/MbDiv.vue
@@ -0,0 +1,236 @@
+<template>
+  <!-- 모바일 메뉴 -->
+  <div class="mb-div">
+    <div class="mb-bg">
+      <div class="mb-top">
+        <span class="mb-top-txt">로그인이 필요합니다.</span>
+        <a href="#" class="login">LOGIN</a>
+        <button class="mb-close"></button>
+      </div>
+      <div class="mb-wrap">
+        <ul class="mb-menu">
+          <li v-for="(item,index) in mbmenu" :key="index">
+            <span class="mb-mainmenu" v-if="item.menuType == 'S'">{{item.mainText}}</span>
+            <a :href="item.mainLink" class="mb-mainmenu" v-if="item.menuType == 'A'">{{item.mainText}}</a>
+            <ul class="mb-submenu" v-if="item.menuType == 'S'">
+              <li v-for="(subitem,subindex) in item.subArr" :key="subindex">
+                <a :href="subitem.link">{{subitem.title}}</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {
+    onMounted
+  } from "vue";
+  import $ from 'jquery';
+
+  export default {
+    props: ['mbmenu'],
+    setup() {
+      // 화면에 html의 구성이 완료되면
+      onMounted(() => {
+        // 모바일메뉴
+        let mb_div = $('.mb-div');
+        console.log(mb_div);
+
+        let mb_bt = $('.mb-bt');
+        mb_bt.click(function () {
+          mb_div.show();
+        });
+
+        let mb_close = $('.mb-close');
+        mb_close.click(function () {
+          mb_div.hide();
+        });
+        // 배경누르면 닫기
+        mb_div.click(function () {
+          mb_div.hide();
+        });
+
+        // 신호 전달 막기
+        $('.mb-bg').click(function () {
+          event.stopPropagation();
+        });
+        // 모바일 메뉴 기능
+        let mb_menu_li = $('.mb-menu > li');
+        $.each(mb_menu_li, function (index) {
+          // mb-mainmenu를 찾아서 보관
+          let temp = $(this).find('.mb-mainmenu');
+          temp.click(function () {
+            // 펼쳐져 있는 경우 true, 아니면 false
+            let result = temp.hasClass('mb-mainmenu-open');
+            if (result == true) {
+              // 펼쳐진 클래스를 가진 경우
+              temp.removeClass('mb-mainmenu-open');
+              // 펼쳐진 서브메뉴 닫기
+              mb_menu_li.find('.mb-submenu').hide();
+
+            } else {
+              // 모든 클라스 일단 제거
+              mb_menu_li.find('.mb-mainmenu').removeClass('mb-mainmenu-open');
+              // 모든 펼쳐진 서브메뉴 닫기
+              mb_menu_li.find('.mb-submenu').hide();
+              // 펼쳐진 클래스가 없는 경우
+              temp.addClass('mb-mainmenu-open');
+              // 서브메뉴 펼치기
+              mb_menu_li.eq(index).find('.mb-submenu').show();
+            }
+          });
+
+
+        });
+        // window 너비 체크
+        $(window).resize(function () {
+          let temp = $(window).width();
+          if (temp >= 880) {
+            mb_div.hide();
+            $('.mb-mainmenu').removeClass('mb-mainmenu-open');
+            $('.mb-submenu').hide();
+          }
+        });
+
+
+      });
+
+
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+  /* 모바일 메뉴 */
+  .mb-div {
+    position: fixed;
+    left: 0;
+    top: 0;
+    display: none;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.2);
+    z-index: 99999;
+    padding-left: 30px;
+  }
+
+  .mb-div::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 380px;
+    height: 100%;
+    background-color: #fff;
+  }
+
+  .mb-top {
+    position: relative;
+    display: block;
+    width: 320px;
+    border-bottom: 1px solid #dcdcdc;
+    padding-top: 35px;
+    padding-bottom: 20px;
+    margin-bottom: 35px;
+  }
+
+  .mb-top-txt {
+    position: relative;
+    display: block;
+  }
+
+  .login {
+    position: relative;
+    display: inline-block;
+    font-size: 14px;
+    color: #ffcc00;
+    margin-top: 15px;
+    font-weight: 500;
+  }
+
+  .mb-close {
+    position: absolute;
+    right: 0;
+    bottom: 20px;
+    display: block;
+    width: 20px;
+    height: 20px;
+    background: url(@/assets/images/btn-close.png) no-repeat center;
+    background-size: contain;
+    border: 0;
+    cursor: pointer;
+    z-index: 9;
+  }
+
+  .mb-wrap {
+    position: relative;
+    display: block;
+    width: 320px;
+  }
+
+  .mb-menu {
+    position: relative;
+    display: block;
+  }
+
+  .mb-menu>li {
+    position: relative;
+    display: block;
+    margin-bottom: 25px;
+  }
+
+  .mb-mainmenu {
+    position: relative;
+    display: block;
+    font-size: 16px;
+    color: #777;
+    font-weight: 500;
+    margin-bottom: 20px;
+    cursor: pointer;
+  }
+
+  .mb-mainmenu::after {
+    content: "";
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 15px;
+    height: 15px;
+    background: url(@/assets/images/ico-gnb-link-m.png) no-repeat center;
+    background-size: contain;
+  }
+
+  .mb-mainmenu-open {
+    color: #ffcc00;
+  }
+
+  .mb-mainmenu-open::after {
+    background: url(@/assets/images/ico-gnb-m-active-up.png) no-repeat center;
+    background-size: contain;
+  }
+
+  .mb-submenu {
+    position: relative;
+    display: none;
+  }
+
+  .mb-submenu li {
+    position: relative;
+    display: block;
+    padding-left: 20px;
+    margin-bottom: 16px;
+  }
+
+  .mb-submenu li a {
+    position: relative;
+    display: block;
+    font-size: 14px;
+  }
+</style>
---
+++ src/components/VisualView.vue
@@ -0,0 +1,234 @@
+<template>
+      <section class="visual">
+        <Swiper
+          :modules="modules"
+          :autoplay = "{
+            delay : 2000,
+            disableOnInteraction: false,
+          }"
+          :loop="true"
+          @swiper="onSwiper"
+          @slideChange="onSlideChange"
+          class="sw-visual"
+        >  
+          <swiper-slide class="swiper-slide" v-for="(item, index) in slideData" :key="index">
+            <VisualList :vimg="item.img" :vtitle="item.title" :vtxt="item.txt" :vbt="item.bt" :vlink="item.link" />
+          </swiper-slide>
+
+        </Swiper>
+    </section>
+</template>
+
+<script>
+import {ref} from 'vue';
+import {Autoplay} from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/vue';
+import 'swiper/css';
+import VisualList from '@/components/VisualList.vue';
+export default {
+  components:{
+    VisualList,Swiper,SwiperSlide
+  },
+  setup(){
+    const onSwiper = (swiper) => {
+      console.log(swiper)
+    };
+    const onSlideChange = () => {
+      console.log('slide change')
+    };
+
+    const slideData = ref([]);
+    slideData.value = [
+      {
+        img: 'PC_minimal_new.png',
+        title : '미니멀비 출시',
+        txt : '아이부터 어른까지 <br> 클린 포뮬러 저자극 케어',
+        bt : 'SHOP',
+        link : '#'
+      },
+      {
+        img: 'PC1_newborn.png',
+        title : 'SPECIAL OFFER',
+        txt : '노엘로힐스가 제안하는 <br> 아기 스킨케어 출산준비물',
+        bt : 'SHOP',
+        link : '#'
+      },
+      {
+        img: 'PC2_BabyEssence.png',
+        title : '밀크 에센스 로션',
+        txt : '국내 최초 100% <br> 식품 그레이드 성분',
+        bt : 'SHOP',
+        link : '#'
+      },
+      {
+        img: 'PC3_Branding.png',
+        title : '8:23 PM',
+        txt : '가족에게 행복을 선사하는 <br> 스킨케어 라이프스타일 브랜드',
+        bt : 'SHOP',
+        link : '#'
+      }
+    ]
+    return{
+      onSwiper,
+      onSlideChange,
+      modules : [Autoplay],
+      SwiperSlide,
+      slideData
+    }
+  },
+}
+</script>
+
+<style>
+.visual {
+  position: relative;
+  display: block;
+}
+.visual .sw-visual {
+  width: 100%;
+  height: 100%;
+}
+.visual .sw-visual .swiper-slide a {
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+}
+.visual .sw-visual .swiper-slide a .visual-img {
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 600px;
+}
+.visual .sw-visual .swiper-slide a .visual-1 {
+  background: url("@/assets/images/PC_minimal_new.png") no-repeat center;
+  background-size: cover;
+}
+.visual .sw-visual .swiper-slide a .visual-2 {
+  background: url("@/assets/images/PC1_newborn.png") no-repeat center;
+  background-size: cover;
+}
+.visual .sw-visual .swiper-slide a .visual-3 {
+  background: url("@/assets/images/PC2_BabyEssence.png") no-repeat center;
+  background-size: cover;
+}
+.visual .sw-visual .swiper-slide a .visual-4 {
+  background: url("@/assets/images/PC3_Branding.png") no-repeat center;
+  background-size: cover;
+}
+.visual .sw-visual .swiper-slide a .visual-txt {
+  position: relative;
+  display: block;
+  text-align: center;
+  padding: 50px 0;
+}
+.visual .sw-visual .swiper-slide a .visual-title {
+  position: relative;
+  display: block;
+  font-size: 30px;
+  color: #777;
+  margin-bottom: 35px;
+  font-weight: 500;
+}
+.visual .sw-visual .swiper-slide a .visual-desc {
+  position: relative;
+  display: block;
+  font-size: 20px;
+  color: #777;
+  margin-bottom: 20px;
+}
+.visual .sw-visual .swiper-slide a .visual-bt {
+  position: relative;
+  display: inline-block;
+  font-size: 20px;
+  color: #777;
+  background-color: #ffcc00;
+  color: #fff;
+  padding: 10px 30px;
+  font-weight: 600;
+}
+.visual .sw-visual .sw-visual-control {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 550px;
+  display: block;
+  z-index: 9;
+}
+.visual .sw-visual .sw-visual-control .sw-visual-pg .swiper-pagination-bullet {
+  width: 10px;
+  height: 10px;
+  background-color: #fff;
+  opacity: 1;
+  margin: 0 4px;
+}
+.visual .sw-visual .sw-visual-control .sw-visual-pg .swiper-pagination-bullet-active {
+  background-color: #f9d14d;
+}
+
+/* 비주얼 반응형 */
+@media all and (max-width: 1320px) {
+  .visual-img {
+    height: 45vw !important;
+  }
+
+  .sw-visual-control {
+    top: 41.66vw !important;
+  }
+
+  .visual-txt {
+    padding: 3.78vw 0 !important;
+  }
+}
+@media all and (max-width: 800px) {
+  .visual-img {
+    height: 125vw !important;
+  }
+
+  .visual-1 {
+    background: url(@/assets/images/mobile-visual-1.png) no-repeat center !important;
+  }
+
+  .visual-2 {
+    background: url(@/assets/images/mobile-visual-2.png) no-repeat center !important;
+  }
+
+  .visual-3 {
+    background: url(@/assets/images/mobile-visual-3.png) no-repeat center !important;
+  }
+
+  .visual-4 {
+    background: url(@/assets/images/mobile-visual-4.png) no-repeat center !important;
+  }
+}
+.sw-visual-control {
+  top: 120vw !important;
+}
+
+@media all and (max-width: 480px) {
+  .visual-img {
+    height: 124.375vw !important;
+  }
+
+  .visual-1 {
+    background: url(@/assets/images/v_480_1.png) no-repeat center !important;
+  }
+
+  .visual-2 {
+    background: url(@/assets/images/v_480_2.png) no-repeat center !important;
+  }
+
+  .visual-3 {
+    background: url(@/assets/images/v_480_3.png) no-repeat center !important;
+  }
+
+  .visual-4 {
+    background: url(@/assets/images/v_480_4.png) no-repeat center !important;
+  }
+
+  .sw-visual-control {
+    top: 115vw !important;
+  }
+}
+</style>
